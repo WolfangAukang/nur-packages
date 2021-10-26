c6633a6083dbd83b830c4924bdb93887dccf4a49
P. R. d. O <d.ol.rod@protonmail.com>
2021-10-26 08:16:51 -0600

    thiefmd: updating to match nixpkgs

--- pkgs/thiefmd/default.nix
+++ pkgs/thiefmd/default.nix
@@ -1,27 +1,21 @@
-{ lib, stdenv, fetchgit, fetchurl, copyDesktopItems, makeDesktopItem, wrapGAppsHook,
-cmake, desktop-file-utils, glib, meson, ninja, pkg-config, vala, clutter, discount,
-gtk3, gtksourceview4, gtkspell3, libarchive, libgee, libhandy, libsecret, link-grammar,
-webkitgtk }:
+{ lib, stdenv, fetchFromGitHub, wrapGAppsHook, cmake, desktop-file-utils, glib
+, meson, ninja, pkg-config, vala, clutter, discount, gtk3, gtksourceview4, gtkspell3
+, libarchive, libgee, libhandy, libsecret, link-grammar, webkitgtk }:
 
-let
-  app_icon = fetchurl {
-    url = "https://thiefmd.com/images/thiefmd_48.png";
-    sha256 = "sha256-0tlP+5mfLSUJ+KV7FnDpYvLyvwKfuKQf6kraHwvlR6k=";
-  };
-
-in stdenv.mkDerivation rec {
+stdenv.mkDerivation rec {
   pname = "thiefmd";
-  version = "0.2.4-easypdf";
+  version = "0.2.4";
 
-  src = fetchgit {
-    url = "https://github.com/kmwallio/ThiefMD.git";
-    rev = "v${version}";
+  src = fetchFromGitHub {
+    owner = "kmwallio";
+    repo = "ThiefMD";
+    rev = "v${version}-easypdf";
     sha256 = "sha256-YN17o6GtpulxhXs+XYZLY36g9S8ggR6URNLrjs5PEoI=";
     fetchSubmodules = true;
   };
 
   nativeBuildInputs = [
-    cmake copyDesktopItems desktop-file-utils glib meson wrapGAppsHook
+    cmake desktop-file-utils glib meson wrapGAppsHook
     ninja pkg-config vala
   ];
 
@@ -33,16 +27,12 @@ in stdenv.mkDerivation rec {
 
   dontUseCmakeConfigure = true;
 
-  desktopItems = makeDesktopItem {
-    name = pname;
-    exec = "thiefmd";
-    icon = app_icon;
-    comment = meta.description;
-    desktopName = "ThiefMD";
-    genericName = "Markdown Editor";
-  };
-
   postInstall = ''
+    mv $out/share/applications/com.github.kmwallio.thiefmd.desktop \
+      $out/share/applications/thiefmd.desktop
+    substituteInPlace $out/share/applications/thiefmd.desktop \
+      --replace 'Exec=com.github.kmwallio.' Exec=$out/bin/
+
     makeWrapper $out/bin/com.github.kmwallio.thiefmd \
       $out/bin/thiefmd \
       --prefix XDG_DATA_DIRS : "${gtk3}/share/gsettings-schemas/${gtk3.name}/"
